fc2bede7e6eacfdb97bde4e91571986e840c03bf
AngeloGabrielS <70040016+AngeloGabrielS@users.noreply.github.com>
2020-10-19 00:23:19 -0300

    Add files via upload

---
+++ prova ivan 1ºB Nivel2/js/fornecedores.js
@@ -0,0 +1,40 @@
+const formulario = document.querySelector('#formularioFornecedores');
+const inputNome = document.querySelector('#nome');
+const inputEmail = document.querySelector('#email');
+const inputProduto = document.querySelector('#produto');
+const fornecedores = document.querySelector('#fornecedores');
+const mensagens = document.querySelector('#mensagens');
+const inputTelefone= document.querySelector('#telefone');
+
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    
+    if (inputNome.value === '' || inputEmail.value === '' || inputProduto.value === '' || inputTelefone.value === '') {
+        mensagens.style.display = 'block';
+        mensagens.style.background = 'red';
+        mensagens.innerHTML = 'Preencha todos os campos!';
+
+    }
+ 
+    else {
+        const li = document.createElement('li');
+        li.innerHTML = `Nome: ${inputNome.value} <br>Email: ${inputEmail.value} <br>Telefone: ${inputTelefone.value} <br>Produto: ${inputProduto.value}`;
+        li.addEventListener('click', (e) => {
+            const deletar = confirm('Você realmente gostaria de deletar este fornecedor?');
+
+            if (deletar) {
+                e.target.remove();
+            }
+        })
+
+        fornecedores.appendChild(li);
+        mensagens.style.display = 'none';
+
+        inputNome.value = '';
+        inputEmail.value = '';
+        inputProduto.value = '';
+        inputTelefone.value = '';
+    }
+})
